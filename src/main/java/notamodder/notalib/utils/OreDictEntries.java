9acda3af43adf487ec2b729c3851090ad0b6f336
NotAModder <modderiamnot@gmail.com>
2017-05-01 18:29:54 -0600

    Added static fields for all the ore dict entries from forge.

---
+++ src/main/java/notamodder/notalib/utils/OreDictEntries.java
@@ -0,0 +1,156 @@
+package notamodder.notalib.utils;
+
+import net.minecraftforge.oredict.OreDictionary;
+
+/**
+ * Contains static fields for all of the standard ore dictionary entries.
+ */
+public class OreDictEntries {
+
+    public static final String LOGWOOD = "logWood";
+    public static final String PLANKWOOD = "plankWood";
+    public static final String SLABWOOD = "slabWood";
+    public static final String STAIRWOOD = "stairWood";
+    public static final String STICKWOOD = "stickWood";
+    public static final String TREESAPLING = "treeSapling";
+    public static final String TREELEAVES = "treeLeaves";
+    public static final String VINE = "vine";
+    public static final String OREGOLD = "oreGold";
+    public static final String OREIRON = "oreIron";
+    public static final String ORELAPIS = "oreLapis";
+    public static final String OREDIAMOND = "oreDiamond";
+    public static final String OREREDSTONE = "oreRedstone";
+    public static final String OREEMERALD = "oreEmerald";
+    public static final String OREQUARTZ = "oreQuartz";
+    public static final String ORECOAL = "oreCoal";
+    public static final String INGOTIRON = "ingotIron";
+    public static final String INGOTGOLD = "ingotGold";
+    public static final String INGOTBRICK = "ingotBrick";
+    public static final String INGOTBRICKNETHER = "ingotBrickNether";
+    public static final String NUGGETGOLD = "nuggetGold";
+    public static final String NUGGETIRON = "nuggetIron";
+    public static final String GEMDIAMOND = "gemDiamond";
+    public static final String GEMEMERALD = "gemEmerald";
+    public static final String GEMQUARTZ = "gemQuartz";
+    public static final String GEMPRISMARINE = "gemPrismarine";
+    public static final String DUSTPRISMARINE = "dustPrismarine";
+    public static final String DUSTREDSTONE = "dustRedstone";
+    public static final String DUSTGLOWSTONE = "dustGlowstone";
+    public static final String GEMLAPIS = "gemLapis";
+    public static final String BLOCKGOLD = "blockGold";
+    public static final String BLOCKIRON = "blockIron";
+    public static final String BLOCKLAPIS = "blockLapis";
+    public static final String BLOCKDIAMOND = "blockDiamond";
+    public static final String BLOCKREDSTONE = "blockRedstone";
+    public static final String BLOCKEMERALD = "blockEmerald";
+    public static final String BLOCKQUARTZ = "blockQuartz";
+    public static final String BLOCKCOAL = "blockCoal";
+    public static final String CROPWHEAT = "cropWheat";
+    public static final String CROPPOTATO = "cropPotato";
+    public static final String CROPCARROT = "cropCarrot";
+    public static final String CROPNETHERWART = "cropNetherWart";
+    public static final String SUGARCANE = "sugarcane";
+    public static final String BLOCKCACTUS = "blockCactus";
+    public static final String DYE = "dye";
+    public static final String PAPER = "paper";
+    public static final String SLIMEBALL = "slimeball";
+    public static final String ENDERPEARL = "enderpearl";
+    public static final String BONE = "bone";
+    public static final String GUNPOWDER = "gunpowder";
+    public static final String STRING = "string";
+    public static final String NETHERSTAR = "netherStar";
+    public static final String LEATHER = "leather";
+    public static final String FEATHER = "feather";
+    public static final String EGG = "egg";
+    public static final String RECORD = "record";
+    public static final String DIRT = "dirt";
+    public static final String GRASS = "grass";
+    public static final String STONE = "stone";
+    public static final String COBBLESTONE = "cobblestone";
+    public static final String GRAVEL = "gravel";
+    public static final String SAND = "sand";
+    public static final String SANDSTONE = "sandstone";
+    public static final String NETHERRACK = "netherrack";
+    public static final String OBSIDIAN = "obsidian";
+    public static final String GLOWSTONE = "glowstone";
+    public static final String ENDSTONE = "endstone";
+    public static final String TORCH = "torch";
+    public static final String WORKBENCH = "workbench";
+    public static final String BLOCKSLIME = "blockSlime";
+    public static final String BLOCKPRISMARINE = "blockPrismarine";
+    public static final String BLOCKPRISMARINEBRICK = "blockPrismarineBrick";
+    public static final String BLOCKPRISMARINEDARK = "blockPrismarineDark";
+    public static final String STONEGRANITE = "stoneGranite";
+    public static final String STONEGRANITEPOLISHED = "stoneGranitePolished";
+    public static final String STONEDIORITE = "stoneDiorite";
+    public static final String STONEDIORITEPOLISHED = "stoneDioritePolished";
+    public static final String STONEANDESITE = "stoneAndesite";
+    public static final String STONEANDESITEPOLISHED = "stoneAndesitePolished";
+    public static final String BLOCKGLASSCOLORLESS = "blockGlassColorless";
+    public static final String BLOCKGLASS = "blockGlass";
+    public static final String PANEGLASSCOLORLESS = "paneGlassColorless";
+    public static final String PANEGLASS = "paneGlass";
+    public static final String CHEST = "chest";
+    public static final String CHESTWOOD = "chestWood";
+    public static final String CHESTENDER = "chestEnder";
+    public static final String CHESTTRAPPED = "chestTrapped";
+    public static final String DYEBLACK = "dyeBlack";
+    public static final String BLOCKGLASSBLACK = "blockGlassBlack";
+    public static final String PANEGLASSBLACK = "paneGlassBlack";
+    public static final String DYERED = "dyeRed";
+    public static final String BLOCKGLASSRED = "blockGlassRed";
+    public static final String PANEGLASSRED = "paneGlassRed";
+    public static final String DYEGREEN = "dyeGreen";
+    public static final String BLOCKGLASSGREEN = "blockGlassGreen";
+    public static final String PANEGLASSGREEN = "paneGlassGreen";
+    public static final String DYEBROWN = "dyeBrown";
+    public static final String BLOCKGLASSBROWN = "blockGlassBrown";
+    public static final String PANEGLASSBROWN = "paneGlassBrown";
+    public static final String DYEBLUE = "dyeBlue";
+    public static final String BLOCKGLASSBLUE = "blockGlassBlue";
+    public static final String PANEGLASSBLUE = "paneGlassBlue";
+    public static final String DYEPURPLE = "dyePurple";
+    public static final String BLOCKGLASSPURPLE = "blockGlassPurple";
+    public static final String PANEGLASSPURPLE = "paneGlassPurple";
+    public static final String DYECYAN = "dyeCyan";
+    public static final String BLOCKGLASSCYAN = "blockGlassCyan";
+    public static final String PANEGLASSCYAN = "paneGlassCyan";
+    public static final String DYELIGHTGRAY = "dyeLightGray";
+    public static final String BLOCKGLASSLIGHTGRAY = "blockGlassLightGray";
+    public static final String PANEGLASSLIGHTGRAY = "paneGlassLightGray";
+    public static final String DYEGRAY = "dyeGray";
+    public static final String BLOCKGLASSGRAY = "blockGlassGray";
+    public static final String PANEGLASSGRAY = "paneGlassGray";
+    public static final String DYEPINK = "dyePink";
+    public static final String BLOCKGLASSPINK = "blockGlassPink";
+    public static final String PANEGLASSPINK = "paneGlassPink";
+    public static final String DYELIME = "dyeLime";
+    public static final String BLOCKGLASSLIME = "blockGlassLime";
+    public static final String PANEGLASSLIME = "paneGlassLime";
+    public static final String DYEYELLOW = "dyeYellow";
+    public static final String BLOCKGLASSYELLOW = "blockGlassYellow";
+    public static final String PANEGLASSYELLOW = "paneGlassYellow";
+    public static final String DYELIGHTBLUE = "dyeLightBlue";
+    public static final String BLOCKGLASSLIGHTBLUE = "blockGlassLightBlue";
+    public static final String PANEGLASSLIGHTBLUE = "paneGlassLightBlue";
+    public static final String DYEMAGENTA = "dyeMagenta";
+    public static final String BLOCKGLASSMAGENTA = "blockGlassMagenta";
+    public static final String PANEGLASSMAGENTA = "paneGlassMagenta";
+    public static final String DYEORANGE = "dyeOrange";
+    public static final String BLOCKGLASSORANGE = "blockGlassOrange";
+    public static final String PANEGLASSORANGE = "paneGlassOrange";
+    public static final String DYEWHITE = "dyeWhite";
+    public static final String BLOCKGLASSWHITE = "blockGlassWhite";
+    public static final String PANEGLASSWHITE = "paneGlassWhite";
+
+    /**
+     * Dumps the contents of the ore dictionary to the console. This isn't actually meant to be
+     * used directly, just storing the code here so it can be used to re-generate the fields of
+     * this class in the future.
+     */
+    public static final void dumpOreDictNames () {
+
+        for (final String name : OreDictionary.getOreNames())
+            System.out.println("public static final String " + name.toUpperCase() + " = \"" + name + "\";");
+    }
+}
