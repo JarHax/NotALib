4b12676c91c3d85e8123968203caba0449ebc450
NotAModder <modderiamnot@gmail.com>
2017-05-02 18:36:54 -0600

    Added a way to get ItemStacks from IBlockStates

--- src/main/java/notamodder/notalib/utils/StackUtils.java
+++ src/main/java/notamodder/notalib/utils/StackUtils.java
@@ -2,6 +2,7 @@ package notamodder.notalib.utils;
 
 import javax.annotation.Nonnull;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.oredict.OreDictionary;
@@ -46,5 +47,17 @@ public final class StackUtils {
     public static boolean areStacksSimilar (@Nonnull ItemStack stack1, @Nonnull ItemStack stack2) {
 
         return stack1.isEmpty() ? stack2.isEmpty() : stack1.getItem() == stack2.getItem() && (stack1.getMetadata() == OreDictionary.WILDCARD_VALUE || stack2.getMetadata() == OreDictionary.WILDCARD_VALUE || stack1.getMetadata() == stack2.getMetadata());
+    }
+
+    /**
+     * Creates an ItemStack which represents an IBlockState.
+     *
+     * @param state The IBlockState to create.
+     * @param size The amount of items to be placed in the stack.
+     * @return An ItemStack which represents the passed IBlockState.
+     */
+    public static ItemStack getStateStack (IBlockState state, int size) {
+
+        return new ItemStack(state.getBlock(), size, state.getBlock().getMetaFromState(state));
     }
 }
